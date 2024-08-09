1f3e0dbad220d60c671b240254472d77286d22f8
Justin Worrall <justin.worrall@gmail.com>
2024-08-09 07:30:35 -0400

    added tests

---
+++ test.py
@@ -0,0 +1,21 @@
+import os, unittest
+
+def find_and_run_tests(root_dirs):
+    suite = unittest.TestSuite()    
+    for root_dir in root_dirs:
+        for root, _, files in os.walk(root_dir):
+            for file in files:
+                if file.endswith('.py'):
+                    full_path = os.path.join(root, file)
+                    module_name = full_path.replace(os.sep, '.')[:-3] 
+                    module = __import__(module_name, fromlist=[''])
+                    for name in dir(module):
+                        obj = getattr(module, name)
+                        if (isinstance(obj, type) and
+                            issubclass(obj, unittest.TestCase)):
+                            print (str(obj)[1:-1].split(" ")[-1][1:-1])
+                            suite.addTest(unittest.TestLoader().loadTestsFromTestCase(obj))
+    unittest.TextTestRunner().run(suite)
+
+if __name__ == "__main__":
+    find_and_run_tests(["tests"])
---
+++ tests/hello.py
@@ -0,0 +1,9 @@
+import unittest
+
+class HelloTest(unittest.TestCase):
+
+    def test_hello(self):
+        self.assertEqual(2, 2)
+
+if __name__ == "__main__":
+    pass
