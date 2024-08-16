0cc6726470ff6ba34f02fe9f49255dc8b3de7529
Justin Worrall <justin.worrall@gmail.com>
2024-08-16 16:48:39 -0400

    removed density and added quantise

--- demos/303/main.py
+++ demos/303/main.py
@@ -8,26 +8,24 @@ import re
 import sys
 
 def bassline(self, n, rand,
-             block_sizes = [2, 3, 4, 5, 6],
+             block_sizes = [2, 3, 4, 5, 6, 7, 8, 9, 10],
              root_offset = -5,
-             note_scale = [-2, 0, 0, 0, 3, 12],
-             # filter_frequencies = ["2000", "3000", "4000", "5000", "6000"],
-             filter_frequencies = ["2000", "2800", "3000", "3800", "4000"],
-             off_density = 1.0):
+             note_scale = [-2, -2, 0, 0, 0, 0, 3, 3, 12],
+             quantise = 4,
+             filter_frequencies = ["2000", "2800", "3000", "3800", "4000", "4800", "5000"]):
     j = 0    
     for i in range(n):
-        if i >= j:
+        if (i >= j and
+            0 == i % quantise):
             block_size = rand["seq"].choice(block_sizes)
             note_offset = root_offset + rand["note"].choice(note_scale)
+            sustain_term = 1 + rand["note"].choice(range(block_size - 1))
             filter_freq = rand["fx"].choice(filter_frequencies)
-            if rand["seq"].random() < off_density:
-                sustain_term = 1 + rand["note"].choice(range(block_size - 1))
-            else:
-                sustain_term = None
             note = self.pluck(note = note_offset,
                               sustain_term = sustain_term, 
                               filter_freq = filter_freq)
-            yield note.render(i = i)
+            if (i + sustain_term) < n:
+                yield note.render(i = i)
             j = i + block_size
                             
 if __name__ == "__main__":
