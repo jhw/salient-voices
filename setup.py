372ed2d3ccc6a5e720c90ab4a13af55ce00010e5
Justin Worrall <justin.worrall@gmail.com>
2024-11-01 19:11:03 +0000

    bumped to 0.3.4

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ def filter_packages(root):
     
 setuptools.setup(
     name="salient-voices",
-    version="0.3.3",
+    version="0.3.4",
     author="jhw",
     author_email="justin.worrall@gmail.com",
     description="A randomising- stem- generating wrapper library for radiant-voices",
