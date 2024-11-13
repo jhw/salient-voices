3aeba8550227bebc67a034369a9f0ce395df88a0
Justin Worrall <justin.worrall@gmail.com>
2024-11-13 08:55:45 +0000

    added ModuleChain.init_form_project

--- dev/decompiler.py
+++ dev/decompiler.py
@@ -5,6 +5,25 @@ from rv.readers.reader import read_sunvox_file
 
 class ModuleChain(list):
 
+    @staticmethod
+    def init_from_project(project):
+        modules = [{"name": mod.name,
+                    "index": mod.index,
+                    "links": {"in": mod.in_links,
+                              "out": mod.out_links}}
+                   for mod in project.modules]
+        module_dict = {module['index']: module['name'] for module in modules}    
+        def dfs(current_index):
+            current_module = module_dict[current_index]
+            if not modules[current_index]['links']['in']:
+                return [ModuleChain([(current_module, current_index)])]
+            chains = []
+            for prev_index in modules[current_index]['links']['in']:
+                for chain in dfs(prev_index):
+                    chains.append(ModuleChain(chain + [(current_module, current_index)]))
+            return sorted(chains, key=lambda x: x[0][1])    
+        return dfs(0)
+    
     def __init__(self, items = []):
         list.__init__(self, items)
 
@@ -20,25 +39,6 @@ class ModuleChain(list):
         return "-".join([f"{name[:3].lower()}-{index}"
                          for name, index in zip(self.names, self.indexes)])
 
-def filter_module_chains(project):
-    modules = [{"name": mod.name,
-                "index": mod.index,
-                "links": {"in": mod.in_links,
-                          "out": mod.out_links}}
-               for mod in project.modules]
-    module_dict = {module['index']: module['name'] for module in modules}
-    
-    def dfs(current_index):
-        current_module = module_dict[current_index]
-        if not modules[current_index]['links']['in']:
-            return [ModuleChain([(current_module, current_index)])]
-        chains = []
-        for prev_index in modules[current_index]['links']['in']:
-            for chain in dfs(prev_index):
-                chains.append(ModuleChain(chain + [(current_module, current_index)]))
-        return sorted(chains, key=lambda x: x[0][1])
-    
-    return dfs(0)
 
 class PatternGroup(list):
 
@@ -69,7 +69,7 @@ def group_timeline(project):
 
 if __name__ == "__main__":
     project = read_sunvox_file("dev/city-dreams.sunvox")
-    mod_chains = filter_module_chains(project)
+    mod_chains = ModuleChain.init_from_project(project)
     pat_groups = group_timeline(project)
     filter_fn = lambda mod_chain, pat_group: mod_chain.indexes[0] in pat_group.mod_indexes
     for mod_chain in mod_chains:
