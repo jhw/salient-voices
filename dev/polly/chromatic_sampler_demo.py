893e97c52f32ff1807fc4a3fc317d7a7026a3d0a
Justin Worrall <justin.worrall@gmail.com>
2024-11-17 15:35:17 +0000

    removed echo

--- dev/polly/chromatic_sampler_demo.py
+++ dev/polly/chromatic_sampler_demo.py
@@ -1,6 +1,5 @@
 from sv.banks import SVBank, SVBanks
 from sv.container import SVContainer
-# from sv.instruments.nine09.samples import Nine09
 
 from sv.instruments import SVInstrumentBase, SVTrigBlock, load_yaml
 from sv.model import SVSlotSampleTrig, SVModTrig, ctrl_value
@@ -11,16 +10,13 @@ import rv.api
 import sys
 import yaml
 
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.INFO,
+                    format="%(levelname)s: %(message)s")
+
 Modules = yaml.safe_load("""
 - name: Beat
   class: sv.sampler.SVSlotSampler
-  links:
-    - Echo
-- name: Echo
-  class: rv.modules.echo.Echo
-  defaults:
-    delay: 36
-    delay_unit: 3 # tick
   links:
     - Output
 """)
@@ -31,29 +27,16 @@ class Nine09(SVInstrumentBase):
     
     def __init__(self, container, namespace, samples,
                  sample_index = 0,
-                 relative_note = 0,
-                 echo_wet = 64, # '2000'
-                 echo_feedback = 64): # '2000'
+                 relative_note = 0):
         super().__init__(container = container,
                          namespace = namespace,
                          root = rv.note.NOTE.C5 + relative_note)
-        self.defaults = {"Echo": {"wet": echo_wet,
-                                  "feedback": echo_feedback}}
-
+        self.defaults = {}
         self.samples = samples
         self.sample_index = sample_index
 
-    def toggle_sample(self):
-        self.sample_index = 1 - int(self.sample_index > 0)
-
     def increment_sample(self):
         self.sample_index = (self.sample_index + 1) % len(self.samples)
-
-    def decrement_sample(self):
-        self.sample_index = (self.sample_index - 1) % len(self.samples)
-        
-    def randomise_sample(self, rand):
-        self.sample_index = rand.choice(list(range(len(self.samples))))
         
     @property
     def sample(self):
@@ -67,33 +50,12 @@ class Nine09(SVInstrumentBase):
                                   vel = volume * level)]
         return SVTrigBlock(trigs = trigs)
 
-    def modulation(self,
-                   level = 1.0,
-                   echo_wet = None,
-                   echo_feedback = None):
-        trigs = []
-        if echo_wet:
-            wet_level = int(level * ctrl_value(echo_wet))
-            trigs.append(SVModTrig(target = f"{self.namespace}Echo/wet",
-                                   value = wet_level))
-        if echo_feedback:
-            trigs.append(SVModTrig(target = f"{self.namespace}Echo/feedback",
-                                   value = echo_feedback))
-        return SVTrigBlock(trigs = trigs)
-    
-if __name__ == "__main__":
-    pass
-
-logging.basicConfig(stream=sys.stdout,
-                    level=logging.INFO,
-                    format="%(levelname)s: %(message)s")
-
 def Speak(self, n, **kwargs):
     for i in range(n):
         if 0 == i % 4:
             trig_block = self.note()
             yield i, trig_block
-            self.increment_sample()
+            # self.increment_sample()
 
 if __name__ == "__main__":
     try:
@@ -108,8 +70,7 @@ if __name__ == "__main__":
         nine09 = Nine09(container = container,
                         namespace = "voice",
                         samples = samples,
-                        relative_note = -12,
-                        echo_wet = 0)
+                        relative_note = -12)
         container.add_instrument(nine09)
         nine09.render(generator = Speak)
         container.write_project("tmp/polly-chromatic-sampler-demo.sunvox")
