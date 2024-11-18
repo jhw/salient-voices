666896145bea65653ca6519c2905454b9a858fbc
Justin Worrall <justin.worrall@gmail.com>
2024-11-18 06:12:13 +0000

    added root to 303 to make demo work

--- sv/instruments/three03/mikey303/__init__.py
+++ sv/instruments/three03/mikey303/__init__.py
@@ -1,17 +1,22 @@
 from sv.instruments import SVInstrumentBase, SVTrigBlock, load_yaml
 from sv.model import SVChromaticSampleTrig, SVNoteOffTrig, SVModTrig
 
+import rv
+import rv.api
+
 class Three03(SVInstrumentBase):
 
     Modules = load_yaml(__file__, "modules.yaml")
     
     def __init__(self, container, namespace, sample,
+                 relative_note = 0,
                  filter_resonance = "575", # no idea re format; doesn't seem to correspond to either of the values in the UI; max seems to be around "599"
                  echo_wet = 64, # '2000'
                  echo_feedback = 64, # '2000'
                  reverb_wet = 2): # setting as integer as easier when wanting tiny amounts only
         super().__init__(container = container,
-                         namespace = namespace)
+                         namespace = namespace,
+                         root = rv.note.NOTE.C5 + relative_note)
         self.sample = sample
         self.defaults = {"Filter": {"resonance": filter_resonance},
                          "Echo": {"wet": echo_wet,
