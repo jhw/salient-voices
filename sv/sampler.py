838e66223d476be1863378fffe6dc56c05a7d80c
Justin Worrall <justin.worrall@gmail.com>
2024-11-23 16:08:50 +0000

    simplified sampler logic

--- sv/sampler.py
+++ sv/sampler.py
@@ -20,13 +20,13 @@ class SVBaseSampler(rv.modules.sampler.Sampler):
     - https://github.com/metrasynth/gallery/blob/master/wicked.mmckpy#L497-L526
     """
 
-    def load_sample(self, src, i):
-        sample = self.Sample()
+    def init_rv_sample(self, src):
+        rv_sample = self.Sample()
         freq, snd = wavfile.read(src)
         if snd.dtype.name == 'int16':
-            sample.format = self.Format.int16
+            rv_sample.format = self.Format.int16
         elif snd.dtype.name == 'float32':
-            sample.format = self.Format.float32
+            rv_sample.format = self.Format.float32
         else:
             raise RuntimeError(f"dtype {snd.dtype.name} Not supported")
         if len(snd.shape) == 1:
@@ -34,37 +34,29 @@ class SVBaseSampler(rv.modules.sampler.Sampler):
             channels = 1
         else:
             size, channels = snd.shape
-        sample.rate = freq
-        sample.channels = {
+        rv_sample.rate = freq
+        rv_sample.channels = {
             1: rv.modules.sampler.Sampler.Channels.mono,
             2: rv.modules.sampler.Sampler.Channels.stereo,
         }[channels]
-        sample.data = snd.data.tobytes()
-        self.samples[i] = sample
-        return sample
+        rv_sample.data = snd.data.tobytes()
+        return rv_sample
 
 class SVSlotSampler(SVBaseSampler):
 
-    """
-    class Sampler:
-        self.note_samples = self.NoteSampleMap()
-        self.samples = [None] * 128
-    class Sampler.Sample:
-        self.relative_note = 16
-    """
-
     def __init__(self, banks, pool, root_note, bpm, max_slots=MaxSlots):
         SVBaseSampler.__init__(self, banks=banks, pool=pool)
-        notes = list(rv.note.NOTE)
-        root = notes.index(root_note)
         self.sample_strings = [str(sample) for sample in self.pool]
+        rv_notes = list(rv.note.NOTE)                
+        root = rv_notes.index(root_note)
         for i, sample in enumerate(self.pool):
-            # Insert sample into slot
-            self.note_samples[notes[i]] = i
+            # init rv sample and insert into self.samples
             src = banks.get_wav(sample)
-            self.load_sample(src, i)
-            # Remap pitch
-            self.samples[i].relative_note += (root + sample.note - i)
+            rv_sample = self.init_rv_sample(src)
+            rv_sample.relative_note += (root + sample.note - i)
+            self.samples[i] = rv_sample
+            # bind rv sample to keyboard/note
+            self.note_samples[rv_notes[i]] = i
 
     def index_of(self, sample):
         return self.sample_strings.index(str(sample))
